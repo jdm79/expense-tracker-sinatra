e3b2a15569af76d90d1736dc020912e44a10e3a5
jdm79 <james.malvern@gmail.com>
2018-10-31 17:40:48 +0000

    test: refactor test to the expenses endpoint, add a config.ru

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -10,18 +10,35 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
-    it 'records submitted expenses' do
-      coffee = {
-        'payee' => 'Starbucks',
-        'amount' => 5.75,
-        'date' => '2017-06-10'
-      }
+    coffee = post_expense{
+      'payee' => 'Starbucks',
+      'amount' => 5.75,
+      'date' => '2017-06-10'
+    }
+    zoo = post_expense{
+      'payee' => 'Zoo',
+      'amount' => 15.25,
+      'date' => '2017-06-10'
+    }
+    groceries = post_expense{
+      'payee' => 'Whole Foods',
+      'amount' => 95.20,
+      'date' => '2017-06-11'
+    }
 
-      post '/expenses', JSON.generate(coffee)
+    it 'records submitted expenses' do
+      # POST coffee, zoo and groceries expenses here
+      get '/expenses/2017-06-10'
+      expect(last_response.status).to eq(200)
+    end
+
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
 
       parsed = JSON.parse(last_response.body)
       expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
     end
   end
 end
